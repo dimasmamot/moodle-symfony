c9b1ed75973995a67b504548494ad78ba38a8041
Mamot <dimas.rizky.hp@gmail.com>
2017-12-16 20:45:36 +0700

    Testing Sonata

---
+++ src/AppBundle/Admin/PaketSoalAdmin.php
@@ -0,0 +1,29 @@
+<?php
+	
+	// src/AppBundle/Admin/PaketSoalAdmin.php
+	namespace AppBundle\Admin;
+
+	use Sonata\AdminBundle\Admin\AbstractAdmin;
+	use Sonata\AdminBundle\DataGrid\ListMapper;
+	use Sonata\AdminBundle\DataGrid\DatagridMapper;
+	use Sonata\AdminBundle\Form\FormMapper;
+
+	/**
+	* 
+	*/
+	class PaketSoalAdmin extends AbstractAdmin	{
+		
+		protected function configureFormFields(FormMapper $FormMapper){
+			$formMapper->add('namaPaketSoal', 'text');
+		}
+
+		protected function configureDatagridFilters(DatagridMapper $datagridMapper){
+			$datagridMapper->add('namaPaketSoal');
+		}
+
+		protected function configureListFields(ListMapper $listMapper){
+			$listMapper->addIdentifier('namaPaketSoal');
+		}
+	}
+
+?>
