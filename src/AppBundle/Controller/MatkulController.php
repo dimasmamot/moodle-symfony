ea4f789d32a2330f536ccc7200b79af70249b039
ADMIN-PC\ADMIN <adamzulkarnain98@gmail.com>
2017-12-28 13:27:24 +0700

    assignment, quiz

--- src/AppBundle/Controller/MatkulController.php
+++ src/AppBundle/Controller/MatkulController.php
@@ -5,15 +5,20 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Constraints as Assert;
 
 use Doctrine\ORM\EntityManagerInterface;
 
+use AppBundle\Entity\TblAssignment;
+use AppBundle\Entity\TblQuiz;
 use AppBundle\Entity\TblSubjects;
+use AppBundle\Entity\TblPaketSoal;
 use AppBundle\Entity\TblCourse;
 use AppBundle\Entity\Modul;
 use AppBundle\Entity\MataKuliah;
 
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
@@ -81,7 +86,7 @@ class MatkulController extends Controller
         if(!$fs->exists($rootDir.'/data-file/'.$matkul))
             $fs->mkdir($rootDir.'/data-file/'.$matkul);
         $direktori = $rootDir.'/data-file/'.$matkul.'/'.$modul;
-
+        // form upload modul
         $form = $this->createFormBuilder()
             ->add('file', FileType::class)
             ->add('upload', SubmitType::class, array('label' => 'upload'))
@@ -99,11 +104,60 @@ class MatkulController extends Controller
         array_shift($files);
         array_shift($files);
 
+        //
+        // form quiz
+        $quiz = new TblQuiz();
+        $repositoryTblCourse = $this->getDoctrine()->getRepository(TblCourse::class);
+        $tblCourse = $repositoryTblCourse->findOneBy(['idCoursePk' => $modul]);
+        $quiz->setIdCoursePk($tblCourse);
+        $form2 = $this->createFormBuilder($quiz)
+            ->add('namaQuiz', TextType::class)
+            ->add('idPaketSoal', EntityType::class, array('class' => TblPaketSoal::class, 'choice_value' => 'ID_PAKET_SOAL','choice_label' => 'NAMA_PAKET_SOAL'))
+
+            ->add('tryAttempt', TextType::class)
+            ->add('durasiQuiz', NumberType::class)
+            ->add('tambah', SubmitType::class, array('label' => 'tambah'))
+            ->getForm();
+        $form2->handleRequest($request);
         
+        if ($form2->isSubmitted() && $form2->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($quiz);
+            $em->flush();
+        }
+
+        $repositoryTblQuiz = $this->getDoctrine()->getRepository(TblQuiz::class);
+        $tblQuiz = $repositoryTblQuiz->findBy(['idCoursePk' => $modul]);
+
+        // form assignment
+        $assignment = new TblAssignment();
+        $assignment->setIdCoursePk($tblCourse);
+        $assignment->setStartDate(new \DateTime());
+        $assignment->setTrySubmitCount(1);
+
+        $form3 = $this->createFormBuilder($assignment)
+            ->add('namaAssignment', TextType::class)
+            ->add('dueDate', DateType::class)
+            ->add('tambah', SubmitType::class, array('label' => 'tambah'))
+            ->getForm();
+        $form3->handleRequest($request);
         
+        if ($form3->isSubmitted() && $form3->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($assignment);
+            $em->flush();
+        }
+        $repositoryTblAssignment = $this->getDoctrine()->getRepository(TblAssignment::class);
+        $tblAssignment = $repositoryTblAssignment->findBy(['idCoursePk' => $modul]);
+
+        $repositoriTblSubjects = $this->getDoctrine()->getRepository(TblSubjects::class);
+        $tblSubjects = $repositoriTblSubjects->findOneBy(['subjectId' => $matkul]);
+
+        $repositoriTblCourse = $this->getDoctrine()->getRepository(TblCourse::class);
+        $tblCourse = $repositoriTblCourse->findOneBy(['idCoursePk' => $modul]);
 
         return $this->render('mata-kuliah/modul.html.twig', [
-            'form' => $form->createView(), 'gambarInput' => $gambarInput, 'files' => $files, 'rootDir' => $rootDir, 'matkul' => $matkul, 'modul' => $modul
+            'form' => $form->createView(), 'form2' => $form2->createView(), 'form3' => $form3->createView(),  'gambarInput' => $gambarInput, 'files' => $files, 'rootDir' => $rootDir, 'matkul' => $matkul, 'modul' => $modul, 'tblQuiz' => $tblQuiz, 'tblAssignment' => $tblAssignment, 'tblSubjects' => $tblSubjects, 'tblCourse' => $tblCourse
         ]);
     }
     /**
